ab9382780782989a15384a6efc71260772452704
Chimexx <mezie.cjo@gmail.com>
2025-06-30 00:04:12 +0100

    fix: reduce bcrypt salt rounds for password hashing in user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -44,7 +44,7 @@ userSchema.pre("save", async function(next) {
   if (!this.isModified("password")) return next();
 
   try {
-    const salt = await bcrypt.genSalt(20);
+    const salt = await bcrypt.genSalt(4);
     this.password = await bcrypt.hash(this.password, salt);
     next();
   } catch (err) {
